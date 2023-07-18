fbc07a0e29b01d12094dcc0efb91b384d7dfa130
Diwakar Jha <diwakar.jha@w3villa.com>
2023-07-18 17:02:47 +0530

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController<ApplicationController
     def new
         @user= User.new
     end
+    def show
+        @user= User.find(params[:id])
+        @articles=@user.articles
+    end
     def update
         @user= User.find(params[:id])
         if @user.update(user_params)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def gravatar_for(user, options={size: 80})
+        email_address=user.email.downcase
+        hash= Digest::MD5.hexdigest(email_address)
+        size= options[:size]
+        image_src="https://randomuser.me/api/portraits/men/23.jpg?s=#{size}"
+        image_tag(image_src, alt: user.username, class: "rounded shadow mx-auto d-block")
+    end
 end
