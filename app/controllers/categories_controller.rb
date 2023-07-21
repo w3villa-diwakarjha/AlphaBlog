25f7e9136ee7d4ee4ea78b4e93f4176fa340597f
Diwakar Jha <diwakar.jha@w3villa.com>
2023-07-21 23:19:39 +0530

    Deploy of Alpha Blog

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController<ApplicationController
+
+    before_action :require_admin, except: [:show, :index]
+    def new
+        @category= Category.new
+    end
+    def index
+        @categories=Category.paginate(page: params[:page], per_page: 5)
+    end
+    def show
+        @category=Category.find(params[:id])
+    end
+    def create
+        @category= Category.new(category_params)
+        if @category.save
+            flash[:notice]="Category was successfully created"
+            redirect_to @category
+
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert]= "only admin ca perform the operation"
+            redirect_to categories_path
+        end
+    end
+end
